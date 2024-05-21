5597c0cbae901c6ae00a22f4ff8d58b59ba806e5
RustingRobot <RustingRobot@protonmail.com>
2024-05-21 21:22:46 +0200

    new id

--- CloudListener.py
+++ CloudListener.py
@@ -4,7 +4,7 @@ from firebase_admin import credentials, firestore
 import datetime
 
 # config variables
-project_id = "1023022765"
+project_id = "1023501055"
 chars = ["gnaw", "edward"]
 max_len = 5
 
