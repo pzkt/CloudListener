22374b3d3d728456a2e2344947562e5b3dd766a2
RustingRobot <RustingRobot@protonmail.com>
2025-05-21 18:52:15 +0200

    v2

--- CloudListener.py
+++ CloudListener.py
@@ -1,10 +1,12 @@
 import scratchattach as scratch3
 import firebase_admin
 from firebase_admin import credentials, firestore
-import datetime
+from datetime import datetime, timezone
 import logging
+import requests
 import sched, time
 from logging.handlers import RotatingFileHandler
+from pprint import pprint
 
 #logging
 logger = logging.getLogger('')
@@ -13,85 +15,158 @@ handler = RotatingFileHandler('logfile.log', maxBytes=1024*1024, backupCount=1)
 logger.addHandler(handler)
 
 # config variables
-project_id = "1023501055"
-chars = ["gnaw", "edward"]
-max_len = 10
+chars = ["arc","co-op","ed","gnaw","phantom","xavier"]
+project_id = "184411641"
+max_len = 20
 
 # firebase init
 cred = credentials.Certificate("key.json")
 firebase_admin.initialize_app(cred)
 database = firestore.client()
-db = database.collection('sfrt_scores')
+sfrt_db = database.collection('sfrt_scores_v2')
 metadata = database.collection('meta').document('sfrt')
-metadata.set({'heartbeat':firestore.SERVER_TIMESTAMP}, merge=True)
+recent_db = sfrt_db.document('recent')
+
+# --- reset boards ---
+""" def reset_boards():
+    for c in chars:
+        sfrt_db.document(c).set({"0":{},"1":{},"2":{}})
+reset_boards()
+exit() """
+# --------------------
+
+# update local information
+userdata = sfrt_db.document('userdata').get().to_dict()
 
 data = {}
-for char in chars:
-    data[char] = db.document(char).get().to_dict()
+for c in chars:
+    data[c] = sfrt_db.document(c).get().to_dict()
 
 #functions
 def log(msg):
-    logger.warning(f"[{datetime.datetime.now()}] {msg} \n")
+    logger.warning(f"[{datetime.now()}] {msg} \n")
+    print(msg)
 
-def var_set(val, user):
-    global data
-    if (user is None):
-        return
-
-    score = int(val[2:2+(int(val[:2]) - 10)])
-    char = char_str(val[-2:-1])
-    diff = diff_str(val[-1:])
-
-    if(char is None or diff is None):
-        return
-    chunk = data[char][diff]
-    doc = None
-    if(user in chunk):
-        if(score > chunk[user]):
-            log("new personal highscore - updating old entry")
-            doc = {diff:{user:score}}
-    else:
-        if(len(chunk) < max_len):
-            log("filling available space - adding new entry")
-            doc = {diff:{user:score}}
-        elif(chunk[min(chunk, key=chunk.get)] < score):
-            log("delete worst entry and add new entry")
-            doc = {diff:{min(chunk, key=chunk.get): firestore.DELETE_FIELD, user:score}}
-    if(doc is None):
-        log("nothing changed")
-    else:
-        db.document(char).set(doc, merge=True)
-        data[char] = db.document(char).get().to_dict()
-        metadata.set({'last_update':firestore.SERVER_TIMESTAMP}, merge=True)
-
+#number to character string converter
 def char_str(nr):
     match nr:
-        case "1":
+        case "01":
             return "gnaw"
-        case "5":
-            return "edward"
+        case "02":
+            return "phantom"
+        case "03":
+            return "arc"
+        case "04":
+            return "xavier"
+        case "05":
+            return "ed"
         case _:
             log(f"ERROR: unknown character id: {nr}")
             return None
 
-def diff_str(nr):
-    match nr:
-        case "1":
-            return "beginner"
-        case "3":
-            return "experienced"
-        case "5":
-            return "insane"
-        case _:
-            log(f"ERROR: unknown difficulty id: {nr}")
-            return None
+#called when a score is recorded. val is the raw data
+def var_set(val, user):
+    if(len(val) < 11):
+        log(f"ERROR: value too short")
+        return
+    #version number control
+    if(val[:2] != "02"):
+        log(f"ERROR: wrong version number: {val[:2]}")
+        return
+    #get chars
+    chars = []
+    chars.append(char_str(val[6:8]))
+    if(val[8:10] != "00"):
+        print(val[8:10])
+        chars.append(char_str(val[8:10]))
+
+    diff = val[5]
+    score = int(val[10:])
+
+    #msg validation
+    if(None in chars):
+        return
+    if(diff not in ["0","1","2"]):
+        log(f"ERROR: invalid difficulty: {diff}")
+        return
+    if(score < 1):
+        log("WARNING: zero score")
+        return
+    log(f"decoded: user: {user}, chars: {chars}, diff: {diff}, score: {score}")
+    add_score(user, chars, diff, score)
+
+#add a username-id key-value pair to firestore for pfps and add to cumulative score
+def add_user_data(user, score):
+    if(user not in userdata):
+        res = requests.get(f"https://api.scratch.mit.edu/users/{user}").json()
+        if("id" not in res):
+            return
+        userdata[user] = {}
+        userdata[user]["id"] = res["id"]
+        userdata[user]["score"] = score
+    else:
+        userdata[user]["score"] += score
+    sfrt_db.document('userdata').set(userdata)
+
+def add_score(user, chars, diff, score):
+    #add to recent scores
+    new_entry = {"user": user, "chars": chars, "diff": diff, "score": score, "time": datetime.now(timezone.utc)}
+    recent_doc = recent_db.get()
+    recents = recent_doc.to_dict().get("scores", [])
+    recents_sorted = sorted(recents, key=lambda x: x.get('time'), reverse=True)[:max_len]
+    recent_db.set({'scores' : [new_entry] + recents_sorted[:max_len-1]})
+
+    #calc cumulative scores and ids for users 
+    add_user_data(user, score)
+
+    #check if score needs to be added to top scores and add it
+    mode = "co-op" if len(chars) != 1 else chars[0]
+
+    #add score to darc counter
+    if(mode != "co-op"):
+        sfrt_db.document('darc_goals').update({mode: firestore.Increment(score)})
+    else:
+        sfrt_db.document('darc_goals').update({chars[0]: firestore.Increment(score // 2)})
+        sfrt_db.document('darc_goals').update({chars[1]: firestore.Increment(score // 2)})
+
+    #data[mode] = sfrt_db.document(mode).get().to_dict() #enable to get local storage resilience
+
+    chunk = data[mode][diff]
+    change = False
+    if(user in chunk):
+        if(score > chunk[user]["score"]):
+            log("new personal highscore - updating old entry")
+            change = True
+    else:
+        if(len(chunk) < max_len):
+            log("filling available space - adding new entry")
+            change = True
+        elif(min([e["score"] for e in chunk.values()]) < score):
+            log("delete worst entry and add new entry")
+            min_score = min(chunk[k]['score'] for k in chunk)
+            worst_keys = [k for k in chunk if chunk[k]['score'] == min_score]
+            worst_key = min(worst_keys, key=lambda k: chunk[k]['time'])
+            del data[mode][diff][worst_key]
+            change = True
+    if(not change):
+        log("nothing changed")
+    else:
+        data[mode][diff][user] = {"score": score, "time": datetime.now(timezone.utc)}
+        if(mode == "co-op"):
+            data[mode][diff][user]["chars"] = chars;
+        sfrt_db.document(mode).set(data[mode])
+
+# --- manually add entry ---
+""" add_score("scratchcat",["xavier"],"0",0)
+#var_set("020102010264", "test")
+exit() """
+# --------------------------
 
 #initialize cloud listeners to scratch.mit.edu and turbowarp.org
 log("cloud listener initializing")
 cloud = scratch3.get_scratch_cloud(project_id)
 s_events = cloud.events()
-last_timestamp = 1716320516
-#t_events = scratch3.TwCloudEvents(project_id, purpose="scratchrunning.com scoreboard", contact="sfrt.default552@passinbox.com")
+last_timestamp = 1747842900240
 
 @s_events.event
 def on_set(event):
@@ -99,16 +174,19 @@ def on_set(event):
     if(event.timestamp > last_timestamp):
         last_timestamp = event.timestamp
         metadata.set({'last_data':firestore.SERVER_TIMESTAMP}, merge=True)
-
-    if (event.var == "CloudUpdate2"):
-        log(f"SCRATCH UPDATE - user: {event.user} - raw data: {event.value} - timestamp: {event.timestamp}")
-        var_set(event.value, event.user)
+        if (event.var == "☁ CloudUpdate2"):
+            log(f"SCRATCH UPDATE - user: {event.user} - raw data: {event.value} - timestamp: {event.timestamp}")
+            var_set(event.value, event.user)
+    else:
+        log("old value")
 
 @s_events.event
 def on_ready():
    log("listening to scratch.mit.edu...")
 
-""" @t_events.event
+#we don't do turbowarp for now
+"""
+@t_events.event
 def on_set(event):
     if (event.var == "CloudUpdate"):
         log(f"TURBOWARP UPDATE - user: {event.user} - raw data: {event.value}")
@@ -116,7 +194,8 @@ def on_set(event):
 
 @t_events.event
 def on_ready():
-   log("listening to turbowarp.org...") """
+   log("listening to turbowarp.org...")
+"""
 
 s_events.start()
 #t_events.start()
